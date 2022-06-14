4b414ebe02b24f67f4be47e2abeb855ba8bd7263
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-06-14 11:25:13 -0400

    Working on repl

--- lib/main.ex
+++ lib/main.ex
@@ -1,9 +1,20 @@
 defmodule Main do
-  def main(["repl"]) do
-    IO.puts("TODO")
+  def main(["-repl"]) do
+    expr = IO.gets("-> ")
+    if expr == "done\n" do
+      IO.puts("i dont want to catch you using a regular calculator")
+    else
+      try do
+        expr |> Smalc.run() |> IO.puts()
+      catch
+        e -> IO.puts("Error: #{Exception.message(e)}")
+      after
+        main(["-repl"])
+      end
+    end
   end
 
-  def main(["eval", expr]) do
+  def main(["-eval", expr]) do
     expr
     |> Smalc.run()
     |> IO.puts()
